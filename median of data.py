9db90a17b0a4e68b0b657efd643afd3a76fd394a
Ciaran Scott <ciaran_scott@icloud.com>
2022-06-27 11:29:48 +0100

    Initial commit

---
+++ median of data.py
@@ -0,0 +1,10 @@
+data = [[1, 5, 7, 12, 36], [0, 0, 1, 3, 10], [12, 16, 18, 23, 100]]
+repeat = True
+row = 0
+while repeat == True:
+	if row > 2:
+		repeat = False
+	row_length = len(data[row])
+	median = (row_length // 2)
+	print(data[row][median])
+	row = row + 1
